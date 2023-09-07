4c266ffc082e9e408451a13d4c9ebb317bbe3e66
raiferdy541 <raihan.ferdyanza@gmail.com>
2023-09-07 16:49:26 +0700

    url https

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
 // const host = import.meta.env.VITE_BASE_API_URL;
 // const host = "http://localhost:51009";
 // const host = "http://be.gembala.sembadafarm.com";
-const host = "http://be-breeding.sinergifarm.id/";
+const host = "https://be-breeding.sinergifarm.id/";
 
 
 const baseApiUrl = `${host}/`;
